9e70a07fc198af78124185d3c6d10362895a58b9
Stephan Wenninger <stephan.wenninger@tuebingen.mpg.de>
2015-01-29 17:51:22 +0100

    Updated Smithers and MainWindow

--- Monocle/MainWindow.xaml.cs
+++ Monocle/MainWindow.xaml.cs
@@ -77,14 +77,14 @@ namespace Monocle
 
             _captureController.SessionManager.ShotSavedSuccess += (sender, e) =>
             {
-                /*
+                
                 // TODO: check if this works on the real kinect, it crashes with the fake setup
                 if (checkBox.IsChecked != true)
                 {
                     _flashDecay.Begin();
                     lblCaptureCount.Content = _captureController.Session.Shots.Where(x => x.Completed).Count();
                 }
-                 */
+                 
             };
 
             _captureController.SessionManager.ShotSavedError += (sender, e) =>
